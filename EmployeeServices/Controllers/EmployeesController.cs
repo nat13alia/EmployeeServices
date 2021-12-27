70ad68301a49f684fb6bfc276fe8c2e1710e2c83
nat13alia <natalia13krawczyk@gmail.com>
2021-12-27 21:24:27 +0100

    Part 5 - Conent Negotiation

---
+++ EmployeeServices/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeServices.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        // GET
+        public IEnumerable<Employee> Get() 
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
